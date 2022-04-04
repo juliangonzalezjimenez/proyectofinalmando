53cc15eea2b7fe91460b78bf1c098c6a51ad7bc5
juliangonzalezjimenez <98742865+juliangonzalezjimenez@users.noreply.github.com>
2022-04-04 11:29:40 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(0)
+    maqueen.motorStop(maqueen.Motors.All)
+})
+radio.setGroup(21)
+basic.showString("Hola")
+music.playMelody("C5 B A G F E D C ", 120)
 basic.forever(function () {
 	
 })
